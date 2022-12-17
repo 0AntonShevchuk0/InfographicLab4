50bc132c2b559d2a6037de725bdc0e854cf7aee8
0AntonShevchuk0 <antonshevchuk2002@gmail.com>
2022-12-17 10:48:17 +0200

    Добавьте файлы проекта.

---
+++ InfographicLab4/Converters/MarginConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace InfographicLab4.Converters
+{
+    public class MarginConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is int)
+            {
+                return new Thickness((int)value, 0, 0, 0);
+            }
+            return new Thickness();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InfographicLab4/UserControls/AnimatedRectangleControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace InfographicLab4
+{
+    public partial class AnimatedRectangleControl : UserControl
+    {
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(AnimatedRectangleControl));
+
+        public string NameText
+        {
+            get { return (string)GetValue(NameTextProperty); }
+            set { SetValue(NameTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty NameTextProperty =
+            DependencyProperty.Register("NameText", typeof(string), typeof(AnimatedRectangleControl), new PropertyMetadata(""));
+
+        public Brush TextColor
+        {
+            get { return (Brush)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextColorProperty =
+            DependencyProperty.Register("TextColor", typeof(Brush), typeof(AnimatedRectangleControl), new PropertyMetadata(new SolidColorBrush() { Color = Color.FromArgb(255, 0, 0, 0) }));
+
+        public int RectangleStartWidth
+        {
+            get { return (int)GetValue(RectangleStartWidthProperty); }
+            set { SetValue(RectangleStartWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty RectangleStartWidthProperty =
+            DependencyProperty.Register("RectangleStartWidth", typeof(int), typeof(AnimatedRectangleControl), new PropertyMetadata(0));
+
+        public int RectangleEndWidth
+        {
+            get { return (int)GetValue(RectangleEndWidthProperty); }
+            set { SetValue(RectangleEndWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty RectangleEndWidthProperty =
+            DependencyProperty.Register("RectangleEndWidth", typeof(int), typeof(AnimatedRectangleControl), new PropertyMetadata(0));
+
+        public Brush RectangleColor
+        {
+            get { return (Brush)GetValue(RectangleColorProperty); }
+            set { SetValue(RectangleColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty RectangleColorProperty =
+            DependencyProperty.Register("RectangleColor", typeof(Brush), typeof(AnimatedRectangleControl), new PropertyMetadata(new SolidColorBrush() { Color = Color.FromArgb(255, 0, 0, 0) }));
+
+        public AnimatedRectangleControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
